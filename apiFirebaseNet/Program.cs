130f7afde4605ed30fe08e144dda54eba242e55c
field <field@DESKTOP-EHF3QL6>
2017-10-01 22:45:22 +0700

    init

---
+++ apiFirebaseNet/Program.cs
@@ -0,0 +1,75 @@
+﻿using FirebaseNet.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apiFirebaseNet
+{
+    class Program
+    {
+        const string YOUR_APP_SERVER_KEY = "AIzaSyCAdAV35kJ5L3lHFRz3MFQVSQyYXGG4Ehk";
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo _char;
+
+            do
+            {
+                Console.Write("press Quit(Q), continous(anykey)");
+                 _char = Console.ReadKey();
+                SendAndroid();
+            } while (_char.KeyChar.ToString().ToUpper()!="Q");
+
+            Console.ReadKey();
+        }
+
+        static void SendAndroid()
+        {
+            Task.Run(async () =>
+            {
+
+                FCMClient client = new FCMClient(YOUR_APP_SERVER_KEY); //as derived from https://console.firebase.google.com/project/
+                var message = new Message()
+                {
+                    To = "DEVICE_ID_OR_ANY_PARTICULAR_TOPIC", //topic example /topics/all
+                    Notification = new AndroidNotification()
+                    {
+                        Body = "Hello World",
+                        Title = "MyApp",
+                    },
+
+                    Data = new Dictionary<string, string>
+                {
+                    { "number", "1" },
+                    { "count", "10" }
+                }
+                };
+
+                var result = await client.SendMessageAsync(message);
+                return result;
+            });
+        }
+        static void SendIOS()
+        {
+            Task.Run(async () =>
+            {
+
+                FCMClient client = new FCMClient(YOUR_APP_SERVER_KEY); //as derived from https://console.firebase.google.com/project/
+
+                var message = new Message()
+                {
+                    To = "DEVICE_ID_OR_ANY_PARTICULAR_TOPIC", //topic example /topics/all
+                    Notification = new iOSNotification()
+                    {
+                        Body = "Hello World",
+                        Title = "MyApp",
+                    }
+                };
+
+                var result = await client.SendMessageAsync(message);
+                return result;
+            });
+        }
+    }
+}
---
+++ apiFirebaseNet/iOSNotification.cs
@@ -0,0 +1,31 @@
+﻿using FirebaseNet.Messaging;
+using Newtonsoft.Json;
+
+namespace apiFirebaseNet
+{
+   internal   class iOSNotification : INotification
+    {
+        [JsonProperty(PropertyName = "title")]
+        public string Title { get; set; }
+        [JsonProperty(PropertyName = "body")]
+        public string Body { get; set; }
+        [JsonProperty(PropertyName = "icon")]
+        public string Icon { get; set; }
+        [JsonProperty(PropertyName = "sound")]
+        public string Sound { get; set; }
+        [JsonProperty(PropertyName = "tag")]
+        public string Tag { get; set; }
+        [JsonProperty(PropertyName = "color")]
+        public string Color { get; set; }
+        [JsonProperty(PropertyName = "click_action")]
+        public string ClickAction { get; set; }
+        [JsonProperty(PropertyName = "body_loc_key")]
+        public string BodyLocKey { get; set; }
+        [JsonProperty(PropertyName = "body_loc_args")]
+        public string BodyLocArgs { get; set; }
+        [JsonProperty(PropertyName = "title_loc_key")]
+        public string TitleLocKey { get; set; }
+        [JsonProperty(PropertyName = "title_loc_args")]
+        public string TitleLocArgs { get; set; }
+    }
+}
